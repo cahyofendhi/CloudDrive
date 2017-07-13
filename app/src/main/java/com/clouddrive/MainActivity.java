dc408bcc50c80b21643d9cefee2355f910672cbc
DESKTOP-J1UU87D\Bekti <cahyofendhi@gmail.com>
2017-07-13 14:01:46 +0700

    add main layout

--- app/src/main/java/com/clouddrive/MainActivity.java
+++ app/src/main/java/com/clouddrive/MainActivity.java
@@ -2,12 +2,44 @@ package com.clouddrive;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnGoogleDrive, btnDropbox, btnOneDrive, btnBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupView();
+    }
+
+    private void setupView() {
+        btnGoogleDrive  = (Button) findViewById(R.id.button_google_drive);
+        btnDropbox      = (Button) findViewById(R.id.button_dropbox);
+        btnOneDrive     = (Button) findViewById(R.id.button_oneDrive);
+        btnBox          = (Button) findViewById(R.id.button_box);
+        btnGoogleDrive.setOnClickListener(this);
+        btnDropbox.setOnClickListener(this);
+        btnOneDrive.setOnClickListener(this);
+        btnBox.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_google_drive:
+                Log.d("Tag", "google drive");
+                break;
+            case R.id.button_dropbox:
+                break;
+            case R.id.button_oneDrive:
+                break;
+            case R.id.button_box:
+                break;
+        }
     }
 }
