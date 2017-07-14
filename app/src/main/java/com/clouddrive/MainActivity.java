4da07ec72bb1ac2696c7964dfc7609bf34cac1eb
DESKTOP-J1UU87D\Bekti <cahyofendhi@gmail.com>
2017-07-14 13:49:57 +0700

    setup dropbox

--- app/src/main/java/com/clouddrive/MainActivity.java
+++ app/src/main/java/com/clouddrive/MainActivity.java
@@ -1,12 +1,23 @@
 package com.clouddrive;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.users.FullAccount;
+import com.dropbox.sync.android.DbxException;
+import com.dropbox.sync.android.DbxFileSystem;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     Button btnGoogleDrive, btnDropbox, btnOneDrive, btnBox;
 
@@ -35,11 +46,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d("Tag", "google drive");
                 break;
             case R.id.button_dropbox:
+                onClickLinkToDropbox(v);
                 break;
             case R.id.button_oneDrive:
                 break;
             case R.id.button_box:
                 break;
         }
+    }
+
+    public void onClickLinkToDropbox(View view) {
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
